bc7d8200b5828608c9eee720534abbd2f84c6816
Dannyelgeek <daot1993@gmail.com>
2022-11-18 15:58:43 -0500

    _printf task 1

--- functions.c
+++ functions.c
@@ -37,9 +37,9 @@ int print_string(char *s)
  */
 int print_number(int n)
 {
-	int n2, i = 0, dig_count = 0;
+	int n2, j = 0, i = 0, dig_count = 0;
 	char num[] = "123456789", dig;
-	
+
 	if (n == 0)
 	{
 		write(1, "0", 1);
@@ -52,7 +52,21 @@ int print_number(int n)
 		n2 = n2 / 10;
 	}
 	n2 = n;
-	for (;i < dig_count; i++)
+	if (n < 0)
+	{
+		n2 = n * -1;
+		for (; j < dig_count; j++)
+		{
+			dig = '0' + (n2 % 10);
+			num[dig_count - j - 1] = dig;
+			n2 = n2 / 10;
+		}
+		write(1, "-", 1);
+		write(1, &num[0], dig_count);
+		return (dig_count + 1);
+	}
+	n2 = n;
+	for (; i < dig_count; i++)
 	{
 		dig = '0' + (n2 % 10);
 		num[dig_count - i - 1] = dig;
