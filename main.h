3d2443aec987b4a4836733aa6f0c292d4b9670de
esteban <juan.esteban.94@outlook.com>
2022-11-22 22:09:43 -0500

    test

--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-//struc definition for all cases
+
 struct cases
 {
 	char *allcases;
@@ -13,15 +13,15 @@ struct cases
 } ;
 typedef struct cases print_cases;
 
-//principal function
+
 int _printf(char *format, ...); 
 
-//cases functions
+
 int perc_case(va_list, char *, int);
 int c_case(va_list, char *, int);
 int s_case(va_list, char *, int);
 
-//auxiliar functions
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int (*get_format_especifier(char *arg))(va_list, char *, int);
 
