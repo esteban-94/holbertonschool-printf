51f07d700c0bbe916a192998c82f0389f57094f1
esteban <juan.esteban.94@outlook.com>
2022-11-23 18:18:37 -0500

    testing hexad

--- cases_functions_2.c
+++ cases_functions_2.c
@@ -100,3 +100,69 @@ int x_case(char *buf, int print_len, va_list val)
 	}
 	return (print_len);
 }
+
+
+int o_case(char *buf, int print_len, va_list val)
+{
+	int n1;
+	unsigned int n2, i = 0, dig_count = 0;
+	char num[33], dig;
+
+	n1 = va_arg(val, int);
+	n2 = n1;
+	do {
+		dig = '0' + (n2 % 8);
+		num[i] = dig;
+		i++;
+		dig_count++;
+		n2 = n2 / 8;
+	} while (n2 != 0);
+	i = 0;
+	while (i < dig_count)
+	{
+		buf[i] = num[dig_count - i - 1];
+		i++;
+		print_len++;
+	}
+	return (print_len);
+}
+
+
+int x_case(char *buf, int print_len, va_list val)
+{
+	int n1;
+	unsigned int n2, dig1, i = 0, j, dig_count = 0;
+	unsigned int max_number = 4294967295, numb[] = {10, 11, 12, 13, 14, 15};
+	char num[33], letters[] = {'A', 'B', 'C', 'D', 'E', 'F'}, dig;
+
+	n1 = va_arg(val, int);
+	if (n1 < 0)
+		n2 = max_number + n1 + 1;
+	else
+		n2 = n1;
+	do {
+		dig1 = n2 % 16;
+			if (dig1 > 9)
+			{
+				for (j = 0; j < 6; j++)
+				{
+					if (dig1 == numb[j])
+						dig = letters[j];
+				}
+			}
+			else
+				dig = '0' + dig1;
+		num[i] = dig;
+		i++;
+		dig_count++;
+		n2 = n2 / 16;
+	} while (n2 != 0);
+	i = 0;
+	while (i < dig_count)
+	{
+		buf[i] = num[dig_count - i - 1];
+		i++;
+		print_len++;
+	}
+	return (print_len);
+}
