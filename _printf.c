96110868e316280799a6276ccb60802903c7ad8a
esteban-94 <114319655+esteban-94@users.noreply.github.com>
2022-11-24 15:09:54 -0500

    testing buffer

--- _printf.c
+++ _printf.c
@@ -9,30 +9,36 @@ int _printf(char *format, ...)
 	int i = 0;
 	unsigned int print_len = 0;
 	va_list a;
-	char *buffer;
+	char buffer[2000];
+	int (*selectioned_case)(char *, int, va_list);
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		exit(1);
-	buffer = malloc(sizeof(char));
 	va_start(a, format);
 	while (format && format[i])
 	{
 		if (format[i] != '%')
 		{
-			buffer = realloc(buffer, (print_len + 2));
 			buffer[print_len] = format[i];
 			print_len++;
 		}
 		else
 		{
-			buffer = realloc(buffer, (print_len + 2));
-			buffer[print_len] = format[i];
-			print_len++;
+			selectioned_case = get_format_especifier(&(format[i + 1]));
+			if (selectioned_case != NULL)
+			{
+				print_len = selectioned_case(&buffer[print_len], print_len, a);
+				i++;
+			}
+			else
+			{
+				buffer[print_len] = format[i];
+				print_len++;
+			}
 		}
 		i++;
 	}
 	write(STDOUT_FILENO, buffer, print_len);
 	va_end(a);
-	free(buffer);
 	return (print_len);
 }
