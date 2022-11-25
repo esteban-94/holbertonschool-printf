fbf29cf0a21a5dd1e490048ba5bd60dbeb9db629
Dannyelgeek <daot1993@gmail.com>
2022-11-25 09:52:34 -0500

    _printf project

--- auxiliar_functions_1.c
+++ auxiliar_functions_1.c
@@ -1,41 +1,5 @@
 #include "main.h"
 /**
- * _realloc - reallocation memory of the buffer.
- * @ptr: pointer to the memory.
- * @old_size: old memory size.
- * @new_size: new memory size.
- * Return: p if memory is free.
-*/
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	char *p;
-	unsigned int i, max = new_size;
-	char *oldp = ptr;
-
-	if (ptr == NULL)
-	{
-		p = malloc(new_size);
-		return (p);
-	}
-	else if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	else if (new_size == old_size)
-		return (ptr);
-
-	p = malloc(new_size);
-	if (p == NULL)
-		return (NULL);
-	if (new_size > old_size)
-		max = old_size;
-	for (i = 0; i < max; i++)
-		p[i] = oldp[i];
-	free(ptr);
-	return (p);
-}
-/**
  * get_format_especifier - get specifier for _printf function.
  * @arg: format argument.
  * Return: the specified format.
